f5ec191b803522014d0b8bcefcd1f1feb0b9d933
cristian-castrillon <cristian.castrillon@gmail.com>
2017-07-13 16:51:37 -0500

    [ADD] Funcionalidad api para update y destroy

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::ProductsController < ApplicationController
     render json: @products
   end
 
-  def new
-    # @product = Product.new
-  end
+  # def new
+  #   @product = Product.new
+  # end
 
   def create
     @product = Product.new(product_params)
@@ -23,12 +23,23 @@ class Api::V1::ProductsController < ApplicationController
      end
   end
 
-  def edit
-    # @product = Product.find(params[:id])
-  end
+  # def edit
+  #   @product = Product.find(params[:id])
+  # end
 
   def update
-    # @product = Product.update(params[:id], product_params)
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      render json: product, status: :ok, location: product
+    else
+      render json: product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    head :no_content
   end
 
   private
