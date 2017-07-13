96c14a5a85e1e04e0f5c70028d6ebe6fe923175f
cristian-castrillon <cristian.castrillon@gmail.com>
2017-07-13 16:57:28 -0500

    [MOD] Pequeña corrección

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,26 +6,18 @@ class Api::V1::ProductsController < ApplicationController
     render json: @products
   end
 
-  # def new
-  #   @product = Product.new
-  # end
-
   def create
-    @product = Product.new(product_params)
+    product = Product.new(product_params)
     respond_to do |format|
        format.json do
-           if @product.save
-              render json: @product, status: :created, location: @product
+           if product.save
+              render json: product, status: :created, location: product
            else
-              render json: @product.errors, status: :unprocessable_entity
+              render json: product.errors, status: :unprocessable_entity
            end
        end
      end
   end
-
-  # def edit
-  #   @product = Product.find(params[:id])
-  # end
 
   def update
     product = Product.find(params[:id])
